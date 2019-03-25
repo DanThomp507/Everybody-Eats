ec2d5ef11e157df45fcac0ecedde1e4ff9edbd85
James Yun <jamesyun@ool-ad0205ef.dyn.optonline.net>
2019-03-25 17:01:17 -0400

    created register form and api helper file. Also created user axios but did not finish

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Link, Route, withRouter } from "react-router-dom";
+import UserForm from './components/UserForm';
 
 class App extends Component {
   constructor(props) {
   super(props);
+
   this.state = {
+
     }
+  }
+  
   render() {
     return (
       <div className="App">
+        <UserForm />
       </div>
     );
   }
---
+++ client/src/components/UserForm.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class UserForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="First Name"
+            name="first_name"
+            value={this.first_name}
+          />
+
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Last Name"
+            name="last_name"
+            value={this.last_name}
+          />
+
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.email}
+          />
+
+          <input
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={this.username}
+          />
+
+          <input
+            onChange={this.handleChange}
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.password}
+          />
+
+          <input value="Submit" type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserForm;
