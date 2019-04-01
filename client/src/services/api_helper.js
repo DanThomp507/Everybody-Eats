9249c8bf71831d8eee5609e3cc6aac8aa14e1c5c
Daniel Thompson <DanThomp507@gmail.com>
2019-04-01 10:26:53 -0400

    fixed heroku

--- client/src/services/api_helper.js
+++ client/src/services/api_helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://everybodyeatsapp.herokuapp.com'
+  baseURL: "https://everybodyeatsapp.herokuapp.com"
 });
 
 const updateToken = (token) => {
